7c0f57202716d68e744116fb535e128d472f6d95
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-08-28 10:52:11 -0700

    basic layout of the home page is up, typography unstyled.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,41 +9,52 @@ export default function Home() {
       <Head>
         <link rel='stylesheet' href={HomePageStyle.styles} />
       </Head>
-
-      <div className={HomePageStyle.container}>
-        <p>South Bay&apos;s Leading Service For Home Music Lessons!</p>
-        <h1 className={HomePageStyle.heading}>
-          Quality Music Teachers In-Home
-        </h1>
-        <h3>
-          Happily helping hundreds of South Bay Area families with music lessons
-          since 2009.
-        </h3>
-        <Image
-          src='/images/HomeShy.jpg'
-          alt='home music lesson'
-          width={500}
-          height={500}
-          layout='intrinsic'
-        ></Image>
-        <ul>
-          <li>
-            <a href='#'>Piano Lessons</a>
-          </li>
-          <li>
-            <a href='#'>Guitar Lessons</a>
-          </li>
-          <li>
-            <a href='#'>Voice Lessons</a>
-          </li>
-          <li>
-            <a href='#'>Drum Lessons</a>
-          </li>
-          <li>
-            <a href='#'>Violin Lessons</a>
-          </li>
-        </ul>
-      </div>
+      <section className={HomePageStyle.hero}>
+        <div className={HomePageStyle.container}>
+          <p>South Bay&apos;s Leading Service For Home Music Lessons!</p>
+          <h1 className={HomePageStyle.heading}>
+            Quality Music Teachers In-Home
+          </h1>
+          <h3>
+            Happily helping hundreds of South Bay Area families with music
+            lessons since 2009.
+          </h3>
+          <Image
+            src='/images/HomeShy.jpg'
+            alt='home music lesson'
+            width={500}
+            height={500}
+            layout='intrinsic'
+          ></Image>
+          <ul className={HomePageStyle.lesson_links_list}>
+            <li>
+              <a className={HomePageStyle.lesson_link} href='#'>
+                Piano Lessons
+              </a>
+            </li>
+            <li>
+              <a className={HomePageStyle.lesson_link} href='#'>
+                Guitar Lessons
+              </a>
+            </li>
+            <li>
+              <a className={HomePageStyle.lesson_link} href='#'>
+                Voice Lessons
+              </a>
+            </li>
+            <li>
+              <a className={HomePageStyle.lesson_link} href='#'>
+                Drum Lessons
+              </a>
+            </li>
+            <li>
+              <a className={HomePageStyle.lesson_link} href='#'>
+                Violin Lessons
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
