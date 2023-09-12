b6307ebce96591e89466f481c549309ab967d1a6
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-09-12 07:37:19 -0700

    cleaned up home page imports that I was not using

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 // import HomePageStyle from './page.module.css';
 import HomePage from './homepage/page';
-import About from './about/page';
-import Teachers from './teachers/page';
-import FAQ from './faq/page';
-import Contact from './contact/page';
 import Nav from './components/nav/nav';
-import Head from 'next/head';
-import Link from 'next/link';
 
 export default function Home() {
   return (
