c527925eb48554a9f5d74c41410c4bd1704b991a
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-09-13 14:45:58 -0700

    trying to post yelp reviews

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,48 +2,26 @@ import styles from './about.module.css';
 import Image from 'next/image';
 
 const getPostsData = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return res.json();
-};
-const getUsersData = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  return res.json();
-};
-
-const getDogData = async () => {
-  const res = await fetch('https://dog.ceo/api/breeds/image/random', {
-    next: {
-      revalidate: 10,
-    },
-  });
+  const res = await fetch(
+    'https://api.yelp.com/v3/businesses/{musicians-mobile-san-jose}/reviews'
+  );
   return res.json();
 };
 
 export default async function About() {
-  const [posts, users, dog] = await Promise.all([
-    getPostsData(),
-    getUsersData(),
-    getDogData(),
-  ]);
+  const posts = await getPostsData();
+
+  console.log(posts);
 
   return (
     <div>
-      <Image src={dog.message} alt='dog' width={300} height={300} />
-      {posts.map((post: any) => {
+      {/* {posts.map((post: any) => {
         return (
           <div key={post.userId}>
             <p>{post.title}</p>
           </div>
         );
-      })}
-
-      {users.map((user: any) => {
-        return (
-          <div key={user.id}>
-            <p>{user.name}</p>
-          </div>
-        );
-      })}
+      })} */}
     </div>
   );
 }
