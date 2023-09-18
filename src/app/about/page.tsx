02cd55b5f723c8bbd54072f57c766b196c39797b
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-09-18 07:54:05 -0700

    removed api call to yelp

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,11 @@
 import styles from './about.module.css';
-import Image from 'next/image';
 
-const getPostsData = async () => {
-  const res = await fetch(
-    'https://api.yelp.com/v3/businesses/{musicians-mobile-san-jose}/reviews'
-  );
-  return res.json();
-};
-
-export default async function About() {
-  const posts = await getPostsData();
-
-  console.log(posts);
-
+export default function About() {
   return (
     <div>
-      {/* {posts.map((post: any) => {
-        return (
-          <div key={post.userId}>
-            <p>{post.title}</p>
-          </div>
-        );
-      })} */}
+      <div>
+        <h2 className={styles.paragraph}>Welcome to the about page</h2>
+      </div>
     </div>
   );
 }
