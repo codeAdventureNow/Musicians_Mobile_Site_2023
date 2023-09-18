c9c3361ff5e120befa9419e09fdbfcdcf122de7d
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-09-18 14:04:03 -0700

    footer nav links up but need styling

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,9 +1,41 @@
 import styles from './footer.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <h3>Footer</h3>
-    </div>
+    <footer className={styles.footer}>
+      <ul className={styles.footer_nav}>
+        <li>
+          <Link className={styles.nav_links} href='/'>
+            Careers
+          </Link>
+          <Link className={styles.nav_links} href='/'>
+            Blog
+          </Link>
+        </li>
+      </ul>
+      <ul className={styles.footer_navtwo}>
+        <li>
+          <Link className={styles.nav_links} href='/'>
+            Terms & Conditions
+          </Link>
+          <Link className={styles.nav_links} href='/'>
+            Privacy Policy
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.logo}>
+        <Link href='/'>
+          <Image
+            width={100}
+            height={100}
+            layout='intrinsic'
+            src='/images/Logo.jpg'
+            alt='musicians mobile logo'
+          />
+        </Link>
+      </div>
+    </footer>
   );
 }
