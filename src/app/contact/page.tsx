63509510668ecf9b32f49a31d79974796d13d90c
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-12 15:32:08 -0700

    first draft of contact form style

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,55 @@
 import styles from './contact.module.css';
 
+import Image from 'next/image';
+
+import navstyles from '../components/nav/nav.module.css';
+
 export default function Contact() {
   return (
-    <div>
-      <div>
-        <h2 className={styles.paragraph}>Welcome to the contact page</h2>
-      </div>
+    <div className={styles.homepage}>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <h1 className={styles.hero_heading}>Contact Us</h1>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <div className={styles.pillar_flex}>
+            <div className={styles.pillar_column}>
+              <h2 className={styles.pillar_heading}>Option 1</h2>
+              <p>
+                Schedule a 10-minute phone consultation. Talk to one of our team
+                members so we can learn more about your music interests and
+                answer any questions.
+              </p>
+            </div>
+            <div className={styles.pillar_column}>
+              <h2 className={styles.pillar_heading}>Option 2 </h2>
+              <p>Call us directly</p>
+              <div className={navstyles.nav_cta}>
+                <br />
+                <a className={navstyles.phone} href='tel:408-960-1100'>
+                  {' '}
+                  <Image
+                    width={20}
+                    height={100}
+                    layout='intrinsic'
+                    src='/images/phone_icon.png'
+                    alt='phone_icon'
+                    className={styles.phone_icon}
+                  />
+                  <p>(408) 960-1100</p>
+                </a>
+              </div>
+            </div>
+            <div className={styles.pillar_column}>
+              <h2 className={styles.pillar_heading}>Option 3 </h2>
+              <p>Fill out our contact form</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
