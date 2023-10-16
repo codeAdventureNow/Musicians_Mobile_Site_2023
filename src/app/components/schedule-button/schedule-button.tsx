8f8ed379bf077ec7c40721edb2fdb2a29fbb4be0
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-16 09:24:38 -0700

    schedule button is now routed to the contact page

--- src/app/components/schedule-button/schedule-button.tsx
+++ src/app/components/schedule-button/schedule-button.tsx
@@ -3,7 +3,7 @@ import styles from './schedule-button.module.css';
 
 export default function ScheduleButton() {
   return (
-    <Link className={styles.cta_button} rel='noreferrer' href='#'>
+    <Link className={styles.cta_button} rel='noreferrer' href='/contact'>
       Schedule A Free Lesson!
     </Link>
   );
