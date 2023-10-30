41ef4070a4e754506621c8e9c011734bf4d9bf89
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-30 16:09:16 -0700

    cleaned up form-input parts that were transferred to clients component

--- src/app/components/form-input/form-input.tsx
+++ src/app/components/form-input/form-input.tsx
@@ -4,35 +4,11 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import schema from '../../lib/form-data-schema';
 import { FormData } from '../../lib/form-data-schema';
-import {
-  collection,
-  addDoc,
-  getDoc,
-  query,
-  QuerySnapshot,
-  onSnapshot,
-  deleteDoc,
-  doc,
-} from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../firebase-config/firebase-config';
-import { useEffect, useState } from 'react';
-
-interface Client {
-  id: string;
-  data: {
-    availability?: string;
-    email: string;
-    fullName: string;
-    instrument: string[];
-    leadSource: string;
-    phone: number;
-    zipCode: number;
-    message?: number;
-  };
-}
+import { useState } from 'react';
 
 const FormInput = () => {
-  const [clients, setClients] = useState<Client[]>([]);
   const [formSubmitted, setFormSubmitted] = useState(false);
   const {
     register,
@@ -50,22 +26,6 @@ const FormInput = () => {
       data,
     });
     setFormSubmitted(true);
-  };
-
-  useEffect(() => {
-    const q = query(collection(db, 'prospects'));
-    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-      let itemsArr: any = [];
-
-      QuerySnapshot.forEach((doc) => {
-        itemsArr.push({ ...doc.data(), id: doc.id });
-      });
-      setClients(itemsArr);
-    });
-  }, []);
-
-  const deleteClient = async (id: string) => {
-    await deleteDoc(doc(db, 'prospects', id));
   };
 
   return (
@@ -252,29 +212,6 @@ const FormInput = () => {
           <input className={styles.submit} type='submit' />
         </form>
       )}
-
-      {/* <ul>
-        {clients.map((item, id) => (
-          <li key={id}>
-            <div className={styles.customerCard}>
-              <span
-                onClick={() => deleteClient(item.id)}
-                className={styles.deleteButton}
-              >
-                X
-              </span>
-              <p>Name: {item.data.fullName}</p>
-              <p>Zip Code: {item.data.zipCode}</p>
-              <p>Email: {item.data.email}</p>
-              <p>Phone: {item.data.phone}</p>
-              <p>Instruments: {item.data.instrument}</p>
-              <p>LeadSource: {item.data.leadSource}</p>
-              <p>Availability: {item.data.availability}</p>
-              <p>Message: {item.data.message}</p>
-            </div>
-          </li>
-        ))}
-      </ul> */}
     </div>
   );
 };
