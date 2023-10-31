47bb0de627bcc75f8f9808d577898106b0d9df63
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-10-31 10:05:12 -0700

    customized client component with edit buttons

--- src/app/components/client-component/client-component.tsx
+++ src/app/components/client-component/client-component.tsx
@@ -12,6 +12,7 @@ import {
 } from 'firebase/firestore';
 import { db } from '../firebase-config/firebase-config';
 import { useEffect, useState } from 'react';
+import { BiEditAlt } from 'react-icons/bi';
 
 interface Client {
   id: string;
@@ -56,18 +57,54 @@ export default function ClientComponent() {
                   onClick={() => deleteClient(item.id)}
                   className={styles.deleteButton}
                 >
-                  X
+                  Delete
                 </span>
               </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  Name: {item.data.fullName}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  Zip Code: {item.data.zipCode}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
 
-              <p>Name: {item.data.fullName}</p>
-              <p>Zip Code: {item.data.zipCode}</p>
-              <p>Email: {item.data.email}</p>
-              <p>Phone: {item.data.phone}</p>
-              <p>Instruments: {item.data.instrument}</p>
-              <p>LeadSource: {item.data.leadSource}</p>
-              <p>Availability: {item.data.availability}</p>
-              <p>Message: {item.data.message}</p>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>Email: {item.data.email}</p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>Phone: {item.data.phone}</p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  Instruments: {item.data.instrument}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  LeadSource: {item.data.leadSource}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  Availability: {item.data.availability}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
+              <div className={styles.field_edit}>
+                <p className={styles.printedFields}>
+                  Message: {item.data.message}
+                </p>
+                <BiEditAlt className={styles.pencil_icon} />
+              </div>
             </div>
           </li>
         ))}
