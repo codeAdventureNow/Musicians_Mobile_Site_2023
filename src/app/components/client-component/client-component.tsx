be600f8eeb3550b913838a91c58e42f944df0b90
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-11-01 11:28:17 -0700

    three or four different attempts at updating fullName hardcoded

--- src/app/components/client-component/client-component.tsx
+++ src/app/components/client-component/client-component.tsx
@@ -9,10 +9,15 @@ import {
   onSnapshot,
   deleteDoc,
   doc,
+  updateDoc,
 } from 'firebase/firestore';
 import { db } from '../firebase-config/firebase-config';
 import { useEffect, useState, useRef } from 'react';
 import { BiEditAlt } from 'react-icons/bi';
+import { FormData } from '../../lib/form-data-schema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import schema from '../../lib/form-data-schema';
 
 interface Client {
   id: string;
@@ -30,6 +35,17 @@ interface Client {
 
 export default function ClientComponent() {
   const [clients, setClients] = useState<Client[]>([]);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      instrument: [],
+    },
+    resolver: zodResolver(schema),
+  });
   useEffect(() => {
     const q = query(collection(db, 'prospects'));
     const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
@@ -46,8 +62,49 @@ export default function ClientComponent() {
     deleteDoc(doc(db, 'prospects', id));
   };
 
+  // const updateData = (id: string) => {
+  //   console.log('submitted');
+  //   updateDoc(doc(db, 'prospects', id), {
+  //     fullName: 'Gilberto Vegas',
+  //   });
+  // };
+
+  // const updateData = async (data: FormData) => {
+  //   console.log('hello');
+  //   const brookeDocRef = doc(db, 'prospects', 'Brooke Sheels');
+  //   console.log('submitted');
+  //   await updateDoc(brookeDocRef, {
+  //     fullName: 'Gilberto Vegas',
+  //   });
+  // };
+  // console.log(clients);
+
+  // const updateData = async (data: FormData) => {
+  //   console.log('hello');
+  //   e.preventDefault();
+
+  //   const docRef = doc(db, 'prospects', id);
+
+  //   await updateDoc(docRef, {
+  //     fullName: 'Yaz Regner',
+  //   });
+  // };
+
+  // const updateData = () => {
+  //   console.log('hello');
+  // };
+
+  // console.log(clients);
+
   return (
     <div>
+      <form onSubmit={setValue(updateData)} className={styles.update}>
+        <label htmlFor='id'>Document id:</label>
+        <input type='text' name='id' />
+
+        <button>Update Name</button>
+      </form>
+
       <ul className={styles.flex}>
         {clients.map((item, id) => (
           <li key={id}>
