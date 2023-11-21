911f632ac636c8e0ea67157a221caf3436b8d28b
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-11-21 09:41:42 -0800

    added intro portion to teachers page and truncated paragraphs

--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -1,12 +1,25 @@
-import Nav from '../components/nav/nav';
 import styles from './teachers.module.css';
 
 export default function Teachers() {
   return (
     <div>
-      <div>
-        <h2 className={styles.paragraph}>Welcome to the teachers page</h2>
-      </div>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <h2 className={styles.section_heading}>Music Teachers and Staff</h2>
+          <p>
+            We believe that when you enjoy your instrument, you stick with it.
+            Our patient, encouraging approach to learning blended with a
+            friendly yet firm teaching style allows you to learn the songs you
+            love. We strive to create an environment of self-motivated practice.
+            <br />
+            <br />
+            We emphasize learning numerous songs/pieces and showcasing them with
+            peers in our exciting performance opportunities. Our teachers are
+            employees who pass proficiency tests, multiple interviews, and a
+            national criminal background check prior to being hired.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
