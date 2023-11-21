1b8e91af7aaf6b179f1c5550150085860b90f9ee
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-11-21 10:06:34 -0800

    intro paragraph has padding a margin-bottom and is now style for mobile

--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -5,19 +5,26 @@ export default function Teachers() {
     <div>
       <section className={styles.section}>
         <div className={styles.container}>
-          <h2 className={styles.section_heading}>Music Teachers and Staff</h2>
-          <p>
-            We believe that when you enjoy your instrument, you stick with it.
-            Our patient, encouraging approach to learning blended with a
-            friendly yet firm teaching style allows you to learn the songs you
-            love. We strive to create an environment of self-motivated practice.
-            <br />
-            <br />
-            We emphasize learning numerous songs/pieces and showcasing them with
-            peers in our exciting performance opportunities. Our teachers are
-            employees who pass proficiency tests, multiple interviews, and a
-            national criminal background check prior to being hired.
-          </p>
+          <h2 className={styles.section_heading}>
+            Music{' '}
+            <span className={styles.hero_heading_highlight}>Teachers</span> +
+            Staff
+          </h2>
+          <div className={styles.intro_paragraph_flex}>
+            <p className={styles.intro_paragraph}>
+              We believe that when you enjoy your instrument, you stick with it.
+              Our patient, encouraging approach to learning blended with a
+              friendly yet firm teaching style allows you to learn the songs you
+              love. We strive to create an environment of self-motivated
+              practice.
+            </p>
+            <p className={styles.intro_paragraph}>
+              We emphasize learning numerous musical pieces and showcasing them
+              with peers in our exciting performance opportunities. Our teachers
+              are employees who pass proficiency tests, multiple interviews, and
+              a background check prior to being hired.
+            </p>
+          </div>
         </div>
       </section>
     </div>
