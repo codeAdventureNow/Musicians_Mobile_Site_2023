765ae5b704be40d68b741c7d2f4707b2d2554159
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-12-08 17:45:17 -0800

    cleaned up nav tests with wait for keywords

--- src/app/components/nav/__test__/Nav.test.tsx
+++ src/app/components/nav/__test__/Nav.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import mockRouter from 'next-router-mock';
 import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 import Nav from '../nav';
 import renderer from 'react-test-renderer';
 
-jest.mock('next/router', () => require('next-router-mock'));
+const user = userEvent.setup();
 
 describe('Nav', () => {
   it('should render the nav bar', () => {
@@ -13,27 +13,23 @@ describe('Nav', () => {
     expect(navBar).toMatchSnapshot();
   });
 
-  //not sure why this doesn't work when I refactor as async/await, or as a fireEvent
-  it('tests that when a user click the logo it will route to home page', () => {
-    const user = userEvent.setup();
+  it('tests that when a user click the logo it will route to home page', async () => {
     render(<Nav />, { wrapper: MemoryRouterProvider });
     user.click(screen.getByTestId('nav-logo'));
-    expect(mockRouter.asPath).toEqual('/');
+    await waitFor(() => expect(mockRouter.asPath).toEqual('/'));
   });
 
-  it('tests that nav link About when clicked routes to the about page', async () => {
-    const user = userEvent.setup();
+  it('tests that "About" link clicked routes to the "about" page', async () => {
     render(<Nav />, { wrapper: MemoryRouterProvider });
-    await user.click(screen.getByTestId('About-id'));
-    expect(mockRouter.asPath).toEqual('/about');
+    user.click(screen.getByTestId('About-id'));
+    await waitFor(() => expect(mockRouter.asPath).toEqual('/about'));
   });
 
-  it('tests that when a user click the schedule button it will route to contact page', async () => {
-    const user = userEvent.setup();
+  it('tests that when a user click the "schedule a free lesson" button it will route to "contact" page', async () => {
     render(<Nav />, {
       wrapper: MemoryRouterProvider,
     });
-    await user.click(screen.getByTestId('schedule-button'));
-    expect(mockRouter.asPath).toEqual('/contact');
+    user.click(screen.getByTestId('schedule-button'));
+    await waitFor(() => expect(mockRouter.asPath).toEqual('/contact'));
   });
 });
