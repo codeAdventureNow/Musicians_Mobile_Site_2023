71975806ec8f9272740b80a5b2794068840cb55b
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-12-13 14:05:28 -0800

    added test to About page that when a user clicks the schedule button it will route to the Contact page

--- src/app/about/__test__/About.test.tsx
+++ src/app/about/__test__/About.test.tsx
@@ -1,9 +1,21 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import About from '../page';
-
+import userEvent from '@testing-library/user-event';
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
+const user = userEvent.setup();
 describe('About', () => {
   it('should render the "About" page', () => {
     const aboutPage = renderer.create(<About />).toJSON();
     expect(aboutPage).toMatchSnapshot();
+  });
+
+  it('tests that when a user click the "schedule a free lesson" button it will route to "contact" page', async () => {
+    render(<About />, {
+      wrapper: MemoryRouterProvider,
+    });
+    user.click(screen.getByTestId('schedule-button'));
+    await waitFor(() => expect(mockRouter.asPath).toEqual('/contact'));
   });
 });
